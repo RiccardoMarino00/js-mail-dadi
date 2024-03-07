b6705c5010b0c922fc3aeaf466da50f0a0d98da0
RiccardoMarino00 <riccardo.marino09@gmail.com>
2024-03-07 17:34:24 +0100

    Added dadi: 2 random number

---
+++ dadi/app.js
@@ -0,0 +1,6 @@
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+  // Numero random giocatore
+     console.log(Math.floor(Math.random()*6)+1)
+  // Numero random computer
+     console.log(Math.floor(Math.random()*6)+1)
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
