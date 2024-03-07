fdf462898156ebbe4aed7d1929d06c7c4858ae67
RiccardoMarino00 <riccardo.marino09@gmail.com>
2024-03-07 22:24:22 +0100

    Added: if/else

--- dadi/app.js
+++ dadi/app.js
@@ -1,12 +1,25 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
   // Numero random giocatore
-     console.log( Math.floor(Math.random()*6) +1)
+     console.log ( Math.floor(Math.random()*6) +1)
   // Numero random computer
      console.log( Math.floor(Math.random()*6) +1)
+     
+let dadoGiocatore = Math.floor(Math.random()*6) +1
+let dadoComputer = Math.floor(Math.random()*6) +1
 
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
-  // Se numero giocatore > di quello del computer allora il giocatore ha vinto
+  // SE dado giocatore è uguale al dado del computer è pareggio
+     if (dadoGiocatore === dadoComputer){
+      console.log("Pareggio")
   // INVECE SE numero giocatore < di quello del computer allora quest'ultimo vince
-  // ALTRIMENTI pareggio
+     } else if (dadoGiocatore < dadoComputer){
+      console.log("Il Computer ha vinto!")
+  // ALTRIMENTI vince il giocatore
+     } else {
+       console.log("Il giocatore ha vinto")
+     }
+   
+     
+
     
  
