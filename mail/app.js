1d2106966c221a6583f0263f369d17a0a506fed0
RiccardoMarino00 <riccardo.marino09@gmail.com>
2024-03-07 16:06:48 +0100

    Added: array, cicle

---
+++ mail/app.js
@@ -0,0 +1,56 @@
+console.log("Js")
+
+// Creo una lista 
+const emailRegistrateArray = ["riccardo@gmail.com", "luigi@outlook.it", "gabriel@hotmail.com"]
+// Chiedi all’utente la sua email
+  // Assegno una costante all'input
+     const inputEmailUser = document.getElementById("email")
+  // Assegno una costante al button
+     const inputButton = document.getElementById("send-button")
+  
+  // Aggiungo un event listener al click sul button
+     inputButton.addEventListener("click", function() {
+        // Creo una variabile di controllo per verificare se email coincide
+           let areSameEmailInArray = false
+            // Creo un ciclo per controllare presenza dell'email
+              for (let i = 0; i < emailRegistrateArray.length; i++){
+                   const emailRegistrataArray = emailRegistrateArray[i]
+             
+
+
+                   // Prendo la value di inputEmailUser
+                     const inputEmailUser = parseInt(email.value)
+            //    console.log(inputEmailUser)
+
+            // Se email utente (inputEmailUser) è uguale a una delle email registrate (emailRegistrateArray)
+               if (inputEmailUser === emailRegistrataArray){
+                  areSameEmailInArray = true                
+               } 
+               
+            
+              } 
+
+              if (areSameEmailInArray === true){
+
+                // Compare messaggio "può accedere"
+                  console.log("Puoi accedere")
+
+
+              } else {
+
+               // Altrimenti Compare messaggio "non può accedere"
+               console.log("Non puoi accedere")
+
+              }
+            
+            
+               
+          
+
+     })
+
+// controlla che email insrita da utente sia nella lista di chi può accedere
+  
+  
+// stampa un messaggio appropriato sull’esito del controllo
+
