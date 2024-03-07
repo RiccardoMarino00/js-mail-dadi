62c94bfb8c37015fe8f2f494ef0bbfb2aeb8c22e
RiccardoMarino00 <riccardo.marino09@gmail.com>
2024-03-07 18:02:32 +0100

    delate: string

--- dadi/app.js
+++ dadi/app.js
@@ -1,14 +1,11 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
   // Numero random giocatore
-     console.log( "Il giocatore ha ottenuto " + Math.floor(Math.random()*6)+1)
+     console.log( Math.floor(Math.random()*6) +1)
   // Numero random computer
-     console.log( "Il computer ha ottenuto " + Math.floor(Math.random()*6)+1)
+     console.log( Math.floor(Math.random()*6) +1)
+
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
   // Se numero giocatore > di quello del computer allora il giocatore ha vinto
-     if (){
-         console.log("Il giocatore ha vinto")
-        // Altrimenti vince il computer
-     } else {
-        console.log("Il computer ha vinto")
-     }
+  // Altrimenti vince il computer
+    
  
