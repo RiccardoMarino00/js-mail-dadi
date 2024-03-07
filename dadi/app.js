5a75587114452bb8055ddeab977c3a656a0d5f44
RiccardoMarino00 <riccardo.marino09@gmail.com>
2024-03-07 17:42:37 +0100

    Added:  "il giocatore/computer ha ottenuto"

--- dadi/app.js
+++ dadi/app.js
@@ -1,6 +1,6 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
   // Numero random giocatore
-     console.log(Math.floor(Math.random()*6)+1)
+     console.log( "Il giocatore ha ottenuto " + Math.floor(Math.random()*6)+1)
   // Numero random computer
-     console.log(Math.floor(Math.random()*6)+1)
+     console.log( "Il computer ha ottenuto " + Math.floor(Math.random()*6)+1)
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
